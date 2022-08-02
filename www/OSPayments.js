1b1b006399a69cdf0c176a04968377beec749c49
Ricardo Silva <97543217+OS-ricardomoreirasilva@users.noreply.github.com>
2022-08-02 15:34:23 +0100

    feat: Add CheckWalletSetup Method (#4)
    Add CheckWalletSetup Method to the javascript bridge file, that can be used by both iOS and Android

--- www/OSPayments.js
+++ www/OSPayments.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.setupConfiguration = function(success, error) {
     exec(success, error, 'OSPayments', 'setupConfiguration');
 };
+
+exports.checkWalletSetup = function(success, error) {
+    exec(success, error, 'OSPayments', 'checkWalletSetup');
+};
